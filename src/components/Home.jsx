c7a9593e628c62aeeec9455ddfb6703ea4fe2903
taiwoj121 <taiwoj121@gmail.com>
2022-10-18 16:22:49 +0100

    home component  -created css style for home section - fixed issues with the form and hero section

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import './styles/home.scss'
+import Banner from '../images/banner.png'
+import Button from '../shared/Button';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="container">
+                <div className='content-sec'>
+                    <h1>Find a <span>job</span> that <span>matches</span> your passion</h1>
+                    <p>Hand-picked opportunities to work from home, remotely, freelance, full-time, part-time, contract and internships.</p>
+                    <form action="">
+                        <div className='input-group'>
+                            <input type="text" placeholder='Search by job titile.....' />
+                           <button> Search</button>
+                        </div>
+                    </form>
+                </div>
+                <div className='hero-img'>
+                    <img src={Banner} alt="" className='' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
