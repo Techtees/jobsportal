8f5ac8e79591b76eec0882d3268315692f208ec2
taiwoj121 <taiwoj121@gmail.com>
2022-10-18 16:57:03 +0100

    job categories component - created categories style

---
+++ src/components/Categories.jsx
@@ -0,0 +1,12 @@
+import './styles/categories.scss'
+function Categories() {
+    return(
+        <div className="categories">
+            <div className="container">
+                <h2 className="heading">Popular Categories</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Categories;
