28a97faacd50c3ad3d4e9b1a7e5ad99ad584d6aa
taiwoj121 <taiwoj121@gmail.com>
2022-10-19 14:30:39 +0100

    ctreated a category list and mapped through -style for the lists

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,9 +1,36 @@
 import './styles/categories.scss'
+import Icon from '../images/icon.svg'
+import {FaBox} from 'react-icons/fa'
+
+const categories= [
+    {name:"Technology"},
+    {name:"Technology"},
+    {name:"Technology"},
+    {name:"Technology"},
+    {name:"Technology"},
+    {name:"Technology"},
+    {name:"Technology"},
+    {name:"Technology"},
+    {name:"Technology"},
+    {name:"Technology"},
+    {name:"Technology"},
+    {name:"Technology"},
+]
 function Categories() {
     return(
         <div className="categories">
             <div className="container">
                 <h2 className="heading">Popular Categories</h2>
+                <div className='categories-list'>
+                    {
+                        categories.map((item,index) => (
+                            <div key={index} className='categorie-list-item'>
+                                <div className='box'><img src={Icon} alt="" className='box-img' /></div>
+                                <h5 className='item-text'>{item.name}</h5>
+                             </div>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
