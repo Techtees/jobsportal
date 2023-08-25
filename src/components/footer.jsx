1b61b3b521a9a3a2dc5b504002344f3d5e7a55a7
taiwoj121 <taiwoj121@gmail.com>
2023-08-25 23:18:24 +0100

    footer section ui

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import Layout from "../shared/Layout";
+
+function Footer(){
+
+    return(
+        <div className="footer-bg">
+            <Layout className = 'container'>
+                <p className="footer-info">© 2022 Jobs Portal. Designed By Wilson Kinyua.</p>
+            </Layout>
+        </div>
+    )
+}
+
+
+export default Footer;
