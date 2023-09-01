2de265908802aebfe8b17c33e17cb2b166865548
taiwoj121 <taiwoj121@gmail.com>
2023-09-01 19:12:02 +0100

    Add Routing to App.js: Enabling Page Navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
+
 import Header from "./components/Header";
+import {BrowserRouter as Router, Routes, Route, Switch} from "react-router-dom";
 import Home from "./components/Home";
 import Categories from "./components/Categories";
 import Joblist from "./components/Joblist";
 import Footer from "./components/footer";
+import JobView from "./components/JobView";
 
 
 function App() {
   return (
-    <>
-     <Header />
-     <main className="">
-      <Home />
-      <Categories />
-      <Joblist />
-       <Footer />
-     </main>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element= {
+          <>
+            <Home />
+            <Categories />
+            <Joblist />
+          </>
+        } />
+        <Route path='/viewjob' element= {<JobView />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
