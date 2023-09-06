305c38f991e3ebc355140d8c5cbd01188d08e672
taiwoj121 <taiwoj121@gmail.com>
2023-09-06 17:46:53 +0100

    Update Footer Structure

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,11 +3,11 @@ import Layout from "../shared/Layout";
 function Footer(){
 
     return(
-        <div className="footer-bg">
+        <footer className="footer-bg">
             <Layout className='container'>
                 <p className="footer-info">© 2022 Jobs Portal. Designed By Wilson Kinyua.</p>
             </Layout>
-        </div>
+        </footer>
     )
 }
 
