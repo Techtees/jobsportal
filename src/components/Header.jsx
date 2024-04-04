98d103dc3fffc940effa439945a8fca289904c47
taiwoj121 <taiwoj121@gmail.com>
2024-04-04 19:46:15 +0100

    auth render conditioning to show profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
+import { useContext } from "react";
 import Layout from "../shared/Layout"
 import {Link} from 'react-router-dom'
 import './styles/header.scss'
 import Button from "../shared/Button";
 import Logo from "../images/nendu.png"
+import AuthContext from "../context/Auth/userAuthenticationcontext";
 
 function Header() {
+    const {authStore} = useContext(AuthContext)
+
+    
     return (
         <Layout className='header-layout'>
             <header className="container">
@@ -12,12 +17,31 @@ function Header() {
                 <img src={Logo} alt="Nendu" className="logo" />
             </Link>
             <div className="menu-right">
-                <Link to='/login'>
-                     <Button  type='button' className='btn-w-b' text='Login' />
-                </Link>
-                <Link to='/create-job'>
-                    <Button  type='button' className='btn-primary' text='Post a job' />
-                </Link>
+
+                {
+                     authStore ? (
+
+                        <>
+                            <Link to='/profile'>
+                                <Button  type='button' className='btn-w-b' text='profile' />
+                            </Link>
+                            <Link to='/create-job'>
+                                <Button  type='button' className='btn-primary' text='Post a job' />
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                        
+                            <Link to='/login'>
+                                <Button  type='button' className='btn-w-b' text='Login' />
+                            </Link>
+                            <Link to='/create-job'>
+                                <Button  type='button' className='btn-primary' text='Post a job' />
+                            </Link>
+                        </>
+                    )
+                }
+                
             </div>
         </header>
         </Layout>
